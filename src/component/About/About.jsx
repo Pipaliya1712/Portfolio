53025a23cf830094a6a6406516d2977340f9aec1
Parth Pipaliya <parthpipaliya1712@gmail.com>
2024-02-14 08:57:16 +0530

    SVGs and Download CV fuctionality created

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './About.css'
 import ShapeImg from '../Images/Shape_crope.png'
 import MyImg from '../Images/my.png'
 import Typed from 'typed.js'
-// import GitBlack from '../SVGs/'
-// import Git from '../SVGs/'
-// import LinkedBlack from '../SVGs/'
-// import Linked from '../SVGs/'
-// import InstaBklack from '../SVGs/'
-// import Insta from '../SVGs/'
+import GitW from '../SVGs/GitW.png'
+import Git from '../SVGs/Git.png'
+import LinkedW from '../SVGs/LinkW.png'
+import Linked from '../SVGs/Link.png'
+import InstaW from '../SVGs/InstaW.png'
+import Insta from '../SVGs/Insta.png'
+import PDF from '../PDFs/CV.pdf'
 
 function about() {
+
+  const download = () => {
+    const pdf = PDF;
+    const link = document.createElement("a");
+    link.href = pdf;
+    link.download = "Parth Pipaliya CV.pdf"; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   const el = React.useRef(null);
 
@@ -23,23 +35,37 @@ function about() {
     });
 
     return () => {
-      // Destroy Typed instance during cleanup to stop animation
       typed.destroy();
     };
   }, [el]);
 
   return (
     <>
-    <div className='flex aic jcc space Aboutcontainer'>
+    <div className='flex aic space Aboutcontainer'>
       <div className='detel'>
         <div className='tcFFFFFF fs27'>Hello, It's Me</div><br />
         <div className='tcFFFFFF fs50'>Parth Pipaliya</div><br />
         <div className='tcFFFFFF fs24'>And I'm skilled in <span ref={el} className='tc00D6B4' /></div><br />
         <div className='tcFFFFFF fs16'>Welcome, This is my official portfolio website.I'm a passionate web developer with a keen interest in competitive programming and data structures & algorithms. </div>
+        <div className='flex jcc'>
+          <div id='gitw' className='svgSpace'>
+            <img src={GitW} alt="" className='git'/>
+            <Link to={'https://github.com/Pipaliya1712'} target="_blank"><img src={Git} alt="" className='gith'/></Link>
+          </div>
+          <div id='linkw' className='svgSpace'>
+            <img src={LinkedW} alt="" className='link' style={{height: '25px'}}/>
+            <Link to={"https://www.linkedin.com/in/parth-pipaliya-945680224/"} target="_blank"><img src={Linked} alt="" className='linkh'/></Link>
+          </div>
+          <div id='instaw' className='svgSpace'>
+            <img src={InstaW} alt="" className='insta' style={{height: '25px'}}/>
+            <Link to={"https://www.instagram.com/parth._.1712/"} target="_blank"><img src={Insta} alt="" className='instah'/></Link>
+          </div>
+        </div>
+        <button onClick={download} className='button'>Download CV</button>
       </div>
       <div className='images'>
         <img src={ShapeImg} alt="lol" className='shape'/>
-        {/* <img src={MyImg} alt="lol" className='my'/> */}
+        <img src={MyImg} alt="lol" className='my'/>
       </div>
     </div>
     </>
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <nav className='pt-28'>
             <div className="flex space flex jcb">
               <div className='p15 navLogo tcFFFFFF'>I'M PARTH PIPALIYA</div>
-              <ul className="flex jcc ls">
+              <ul className="flex ls">
                   <li> <NavLink to="" className={({isActive}) => `p20 ${isActive? "tc00D6B4" : "tc999999 hc" } navlink`}> ABOUT </NavLink></li>
                   <li> <NavLink to="portfolio" className={({isActive}) => `p20 ${isActive? "tc00D6B4" : "tc999999 hc" } navlink`}> PORTFOLIO </NavLink></li>
                   <li> <NavLink to="contact" className={({isActive}) => `p20 ${isActive? "tc00D6B4" : "tc999999 hc" } navlink`}> CONTACT </NavLink></li>
